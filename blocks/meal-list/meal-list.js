7f4d64ce9beff3b04e939abf1dc69441715f2842
Anand Parmar <anand.parmar@brainvire.com@BVIAHMLAP-659.brainvire.com>
2026-06-10 16:57:50 +0530

    Meal list dropins added

--- blocks/meal-list/meal-list.js
+++ blocks/meal-list/meal-list.js
@@ -2,8 +2,6 @@ import { render as myRenderer } from '../../scripts/__dropins__/demo-dropin/rend
 import { DropinContainer } from '../../scripts/__dropins__/demo-dropin/containers/DropinContainer.js';
 
 export default async function decorate(block) {
-  console.log('meal-list render');
   block.innerHTML = '';
   await myRenderer.render(DropinContainer, {})(block);
-  console.log('meal-list render completed');
 }
