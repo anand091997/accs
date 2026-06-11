8f0cc76429a29aa7b66fe00421430a8f966ded1f
Anand Parmar <anand.parmar@brainvire.com@BVIAHMLAP-659.brainvire.com>
2026-06-11 10:55:12 +0530

    dropin name changes

--- blocks/meal-list/meal-list.js
+++ blocks/meal-list/meal-list.js
@@ -1,7 +1,7 @@
 import { render as myRenderer } from '../../scripts/__dropins__/meal-dropin/render.js';
-import { DropinContainer } from '../../scripts/__dropins__/meal-dropin/containers/MealContainer.js';
+import { MealContainer } from '../../scripts/__dropins__/meal-dropin/containers/MealContainer.js';
 
 export default async function decorate(block) {
   block.innerHTML = '';
-  await myRenderer.render(DropinContainer, {})(block);
+  await myRenderer.render(MealContainer, {})(block);
 }
